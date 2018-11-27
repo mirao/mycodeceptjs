a4885e60e589a3152db057c633b0d3fcd3cb1136
Jaromir Obr <jaromir.obr@gmail.com>
2018-11-27 20:42:19 +0100

    Added REST examples Related issue: https://github.com/Codeception/CodeceptJS/issues/1072

---
+++ Rest_test.js
@@ -0,0 +1,24 @@
+
+Feature('Rest');
+
+Scenario('Test API', async (I) => {
+    I.haveRequestHeaders({
+        'Content-Type': 'application/json',
+    });
+    const ret = await I.sendPostRequest('/login', JSON.stringify({
+        username: "username",
+        password: "pass"
+    }));
+    
+    console.log(ret.body);
+
+    const ret2 = await I.sendPostRequest('/login', {
+        username: "username",
+        password: "pass"
+    }, {
+        'Content-Type': 'application/json',
+    });
+    
+    console.log(ret2.body);
+
+});
