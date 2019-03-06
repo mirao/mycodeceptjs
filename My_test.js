7133a3eeb9397700ad9297092002073506fc4d86
Jaromir Obr <jaromir.obr@gmail.com>
2019-03-06 21:40:32 +0100

    Added more reliable rewriting of text Added double cllicking of text

--- My_test.js
+++ My_test.js
@@ -6,8 +6,10 @@ Scenario("One", async (I) => {
     I.amOnPage("https://jobr2.bmdcon.com/");
     I.click("#username");
     await I.typeText("Ahoj");
+    I.wait(1);
     await I.pressKey(["Control Left", "a", "Control Left"]);
     await I.typeText("Cau");
+    I.doubleClick("#username");
     pause();
     
 });
